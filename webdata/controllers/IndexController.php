12ffd3f091969955f286ec404b913c59c111992e
Youchen Lee <copyleft@utcr.org>
2014-09-19 18:51:03 +0800

    use media name as DocumentType (will show as "Source" in SMA)

--- webdata/controllers/IndexController.php
+++ webdata/controllers/IndexController.php
@@ -172,7 +172,7 @@ EOF;
             $newArr["Published"] = date("Y-m-d H:i:s", $arr["time"]);
             $newArr["SubjectHtml"] = $arr["title"];
             $newArr["TextHtml"] = $arr["body"];
-            $newArr["DocumentType"] = "news";
+            $newArr["DocumentType"] = $sourceMap[$arr["source"]];
             $newArr["SiteUrl"] = $arr["url"];
             $newArr["SiteName"] = $sourceMap[$arr["source"]];
             $newArr["Language"] = "Chinese - Traditional";
